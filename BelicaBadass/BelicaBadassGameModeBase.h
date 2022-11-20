16496793cf0c6828612f64fcae604b6b12053d03
Michael Walker <pythonk@email.phoenix.edu>
2022-11-20 16:04:08 -0500

    Project Setup - Section 1
    Created the project in Unreal Engine 5 with basic setup. This project will be coded in Visual Studios 2022 using c++. Please note only the c++ source files will be saved to this repository. No content from Unreal Engine will be a part of this repository as the file sizes are way to large.

---
+++ BelicaBadass/BelicaBadassGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BelicaBadassGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BELICABADASS_API ABelicaBadassGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# BelicaBadass
+
+Developed with Unreal Engine 5 and coded with Visual Studio 2022
